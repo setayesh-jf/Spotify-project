8dffd00964c4b45ad8248870c871930e55249366
setayesh-jf <setayeshsadafiii@gmail.com>
2025-04-18 12:27:31 +0330

    create userbehavior for regular and premium user

---
+++ src/Spotify/UserBehavior.java
@@ -0,0 +1,7 @@
+package Spotify;
+
+public interface UserBehavior {
+    void createPlaylist (String Title, User Owner);
+    void playMusic (Music music);
+    void buyPremium (User owner, int month);
+}
